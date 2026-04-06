069a9d557e91c32533eda3370ea2757fe77e3ef5
JULFYKO <bkonarivskij@gmail.com>
2026-04-06 20:29:20 +0300

    add

--- 06.04.2026.py
+++ 06.04.2026.py
@@ -10,20 +10,38 @@ data = {
     'OrderDate': ['2023-06-01', '2023-06-03', '2023-06-05']
 }
 
+# 1
 df = pd.DataFrame(data)
 df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+
+# 2
 df['TotalAmount'] = df['Quantity'] * df['Price']
 
+# 3
+# a
 print(df['TotalAmount'].sum())
+
+# b
 print(df['TotalAmount'].mean())
+
+# c
 print(df.groupby('Customer')['OrderID'].count())
 
+# 4
 print(df[df['TotalAmount'] > 500])
 
+# 5
 print(df.sort_values(by='OrderDate', ascending=False))
 
+# 6
 print(df[df['OrderDate'].between('2023-06-05', '2023-06-10')])
 
-print(df.groupby('Category').agg({'Quantity': 'sum', 'TotalAmount': 'sum'}))
+# 7
+# a
+print(df.groupby('Category')['Quantity'].sum())
 
+# b
+print(df.groupby('Category')['TotalAmount'].sum())
+
+# 8
 print(df.groupby('Customer')['TotalAmount'].sum().nlargest(3))
